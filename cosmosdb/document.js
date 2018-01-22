49a29dd60743b2798e324e40c0daa75b1e94ece6
zj <suke.shou@gmail.com>
2018-01-22 23:07:58 +0800

    Add document.query

--- cosmosdb/document.js
+++ cosmosdb/document.js
@@ -6,8 +6,12 @@ exports.conn = (endPoint, accountKey) => (
 
 exports.dao = (conn, database, collection) => ({
   insert: data => insert(conn, database, collection, data),
+  update: (id, data) => update(conn, database, collection, id, data),
+  remove: id => remove(conn, database, collection, id),
   findById: id => findById(conn, database, collection, id),
   findFirst: query => findFirst(conn, database, collection, query),
+  find: query => find(conn, database, collection, query),
+  query: query => query(conn, database, collection, query),
 })
 
 function findById(conn, database, collection, id) {
@@ -28,6 +32,24 @@ function insert(conn, database, collection, data) {
   })
 }
 
+function update(conn, database, collection, id, data) {
+  const documentLink = `dbs/${database}/colls/${collection}/docs/${id}`
+  return new Promise((resolve, reject) => {
+    conn.replaceDocument(documentLink, data, (err, resource) => {
+      err ? reject(err) : resolve(resource)
+    })
+  })
+}
+
+function remove(conn, database, collection, id) {
+  const documentLink = `dbs/${database}/colls/${collection}/docs/${id}`
+  return new Promise((resolve, reject) => {
+    conn.deleteDocument(documentLink, (err, resource) => {
+      err ? reject(err) : resolve(resource)
+    })
+  })
+}
+
 function findFirst(conn, database, collection, query) {
   const documentLink = `dbs/${database}/colls/${collection}`
   return new Promise((resolve, reject) => {
@@ -35,4 +57,18 @@ function findFirst(conn, database, collection, query) {
       err ? reject(err) : resolve(item)
     })
   })
+}
+
+function find(conn, database, collection, query) {
+  const documentLink = `dbs/${database}/colls/${collection}`
+  return new Promise((resolve, reject) => {
+    conn.queryDocuments(documentLink, query).toArray((err, items) => {
+      err ? reject(err) : resolve(items)
+    })
+  })
+}
+
+function query(conn, database, collection, query) {
+  const documentLink = `dbs/${database}/colls/${collection}`
+  return conn.queryDocuments(documentLink, query)
 }
